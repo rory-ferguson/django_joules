7926fee29c700a0fc1eb77e44cd19133e48263fd
constuelo <ferguson.rory@gmail.com>
2019-02-04 16:44:11 +0000

    Update

--- VENV/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\rory.ferguson\repositories\github\django_joules\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
