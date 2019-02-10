4ba545ed6bc8d1921faf0781a50edfc0583e8725
[rory] <[ferguson.rory@gmail.com]>
2019-02-10 10:21:32 +0000

    Added badges app

---
+++ joules/badges/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .forms import SubmitButtonWidget
+from multiprocessing import Process, Pool
+
+live = [['https://www.joules.com'], ['https://www.joulesusa.com'], ['https://www.tomjoule.de']]
+staging = [['https://uk-staging.prod.joules.joules-prod01.aws.eclipsegroup.co.uk/'], ['https://us-staging.prod.joules.joules-prod01.aws.eclipsegroup.co.uk/'], ['https://de-staging.prod.joules.joules-prod01.aws.eclipsegroup.co.uk/']]
+uk, us, de = [], [], []
+lst = {}
+
+def main(request):
+    if request.method == 'POST':
+        if 'live' in request.POST.get('env'):
+            env = request.POST.get('env')
+
+            with Pool(processes=3) as pool:
+                p1 = pool.apply_async(run_script, (live[0], ))
+                p2 = pool.apply_async(run_script, (live[1], ))
+                p3 = pool.apply_async(run_script, (live[2], ))
+
+                lst['uk'] = p1.get()
+                lst['us'] = p2.get()
+                lst['de'] = p3.get()
+
+                return JsonResponse(lst)
+        if 'staging' in request.POST.get('env'):
+            env = request.POST.get('env')
+
+            with Pool(processes=3) as pool:
+                p1 = pool.apply_async(run_script, (staging[0], ))
+                p2 = pool.apply_async(run_script, (staging[1], ))
+                p3 = pool.apply_async(run_script, (staging[2], ))
+
+                lst['uk'] = p1.get()
+                lst['us'] = p2.get()
+                lst['de'] = p3.get()
+
+                return JsonResponse(lst)
+
+        def envir():
+            pass
+
+
+    else:
+        form = SubmitButtonWidget()
+
+    return render(request, 'missing_categories/content.html', {'form': form})
