4fe045363af7689a453b2a85585b55aa5d1e5047
rory <ferguson.rory@gmail.com>
2019-03-29 22:01:10 +0000

    Update

---
+++ joules/badges/static/badges/scripts/main.js
@@ -0,0 +1,75 @@
+
+// Submit post on submit
+$('#content form').on('submit', function(event){
+    event.preventDefault();
+    $('#content div.uk i').removeClass('hidden')
+});
+
+function validateFunc() {
+    formData = $('form').serializeArray();
+    env = formData[1].value
+    create_post(env)
+    return false;
+}
+
+function create_post(env) {
+    var csrftoken = Cookies.get('csrftoken');
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+    $.ajax({
+        type: "POST",
+        url : "",
+        data : { env : env },
+        async: true,
+        // https://stackoverflow.com/questions/16086162/handle-file-download-from-ajax-post
+        success: function(response, status, xhr) {
+            // check for a filename
+            var filename = "";
+            var disposition = xhr.getResponseHeader('Content-Disposition');
+            if (disposition && disposition.indexOf('attachment') !== -1) {
+                var filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+                var matches = filenameRegex.exec(disposition);
+                if (matches != null && matches[1]) filename = matches[1].replace(/['"]/g, '');
+            }
+
+            var type = xhr.getResponseHeader('Content-Type');
+            var blob = new Blob([response], { type: type });
+
+            if (typeof window.navigator.msSaveBlob !== 'undefined') {
+                // IE workaround for "HTML7007: One or more blob URLs were revoked by closing the blob for which they were created. These URLs will no longer resolve as the data backing the URL has been freed."
+                window.navigator.msSaveBlob(blob, filename);
+            } else {
+                var URL = window.URL || window.webkitURL;
+                var downloadUrl = URL.createObjectURL(blob);
+
+                if (filename) {
+                    // use HTML5 a[download] attribute to specify filename
+                    var a = document.createElement("a");
+                    // safari doesn't support this yet
+                    if (typeof a.download === 'undefined') {
+                        window.location = downloadUrl;
+                    } else {
+                        a.href = downloadUrl;
+                        a.download = filename;
+                        document.body.appendChild(a);
+                        a.click();
+                    }
+                } else {
+                    window.location = downloadUrl;
+                }
+            }
+            // Hide spinner
+            $('#content div.uk i').addClass('hidden')
+        }
+    });
+};
