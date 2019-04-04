8ecf68b3b7c8a580670f688c2fe0d0f89ac6d6d1
rory <ferguson.rory@gmail.com>
2019-04-04 19:40:10 +0100

    Update

--- joules/badges/forms.py
+++ joules/badges/forms.py
@@ -9,11 +9,7 @@ from django.utils import html
 
 class SubmitButtonWidget(forms.Form):
     COUNTRY=[('live','LIVE'),('staging','STAGING')]
-
-    ENV = forms.ChoiceField(choices=COUNTRY, widget=forms.RadioSelect)
-
-
-class SubmitButtonWidgetOne(forms.Form):
     TYPE=[('plp','PLP')]
 
+    ENV = forms.ChoiceField(choices=COUNTRY, widget=forms.RadioSelect)
     PAGE_TYPE = forms.ChoiceField(choices=TYPE, widget=forms.RadioSelect)
--- joules/badges/views.py
+++ joules/badges/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .forms import SubmitButtonWidget
-from .site_scrape_plp.main import run_script
 from multiprocessing import Process, Pool
-from pathlib import Path
-import os.path
-
-from openpyxl import Workbook
-from openpyxl.writer.excel import save_virtual_workbook
-
 import csv
+from .site_scrape_plp.main import run_script
 
 live = [['https://www.joules.com']]
 staging = [['https://uk-staging.prod.joules.joules-prod01.aws.eclipsegroup.co.uk/'], ['https://us-staging.prod.joules.joules-prod01.aws.eclipsegroup.co.uk/'], ['https://de-staging.prod.joules.joules-prod01.aws.eclipsegroup.co.uk/']]
@@ -20,7 +14,6 @@ def main(request):
     if request.method == 'POST':
         if 'live' in request.POST.get('env'):
             lst = run_environment(live)
-            print(lst)
             response = HttpResponse(content_type='text/csv')
             response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
 
@@ -29,28 +22,12 @@ def main(request):
                 writer.writerow(i)
 
             return response
-            
-            # write list to csv
-            # with open('file.csv', 'w') as file:
-            #     for item in my_list:
-            #         file.write("%s\n" % item)
-
-            # content = Path(os.path.abspath(os.path.dirname(__file__))).joinpath('txt.txt')
-            # with open(content, 'rb') as fh:
-            #     response = HttpResponse(fh.read(), content_type='text/csv')
-            #     response['Content-Disposition'] = 'attachment; filename=txt.txt'
-            #     return response
-
-            # file = Path(os.path.abspath(os.path.dirname(__file__))).joinpath('test.xlsx')
-            # with open(file, 'rb') as fh:
-            #     response = HttpResponse(fh.read(), content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-            #     response['Content-Disposition'] = 'attachment; filename=' + str(file)
-            #     return response
 
     else:
         form = SubmitButtonWidget()
 
     return render(request, 'badges/content.html', {'form': form})
+
 
 def run_environment(env):
     with Pool(processes=1) as pool:
