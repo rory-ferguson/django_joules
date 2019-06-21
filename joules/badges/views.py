062630ebde3cbcc29f4e79a163f246037574025b
constuelo <ferguson.rory@gmail.com>
2019-06-21 15:58:15 +0100

    Update2

--- joules/badges/views.py
+++ joules/badges/views.py
@@ -8,18 +8,18 @@ from .site_scrape_plp.main import run_script
 live = [['https://www.joules.com']]
 staging = [['https://uk-staging.prod.joules.joules-prod01.aws.eclipsegroup.co.uk/'], ['https://us-staging.prod.joules.joules-prod01.aws.eclipsegroup.co.uk/'], ['https://de-staging.prod.joules.joules-prod01.aws.eclipsegroup.co.uk/']]
 uk, us, de = [], [], []
-lst = {}
+
 
 def main(request):
     if request.method == 'POST':
         if 'live' in request.POST.get('env'):
-            lst = run_environment(live)
+            rows = run_environment(live[0])
             response = HttpResponse(content_type='text/csv')
             response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
-
+            
             writer = csv.writer(response)
-            for i in lst:
-                writer.writerow(i)
+            for row in rows:
+                writer.writerow(row)
 
             return response
 
@@ -31,6 +31,6 @@ def main(request):
 
 def run_environment(env):
     with Pool(processes=1) as pool:
-        p1 = pool.apply_async(run_script, (env[0], ))
+        p1 = pool.apply_async(run_script, (env, ))
 
         return p1.get()
